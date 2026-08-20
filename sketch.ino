3d42412f95e4b38a205208b00798216eb88280eb
Yusuf-Hafid <yusufhudin504@gmail.com>
2026-08-20 11:01:09 +0700

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // letakkan kode pengaturan Anda di sini, untuk dijalankan satu kali:
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // letakkan kode utama Anda di sini untuk dijalankan berulang kali:
+  digitalWrite(13, HIGH);
+}
